a5ae6b7534702cb7b0a9b284c7457e69d1606919
diego uzcategui <diegoalejandrouzcategui@gmail.com>
2022-07-24 21:06:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let header = document.getElementById('header')
+
+let nube1 = document.getElementById('nube1')
+let transparente = document.getElementById('transparente')
+let nube2 = document.getElementById('nube2')
+let slogan = document.getElementById('slogan')
+let btn = document.getElementById('btn')
+let viento = document.getElementById('viento')
+let persona = document.getElementById('persona')
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    transparente.style.marginLeft =  value * 0.7 + 'px';
+    nube1.style.Left = value * -5 + 'px';
+    nube2.style.Left = value * -5 + 'px';
+    bnt.style.marginTop = value * 0.4 + 'px';
+    slogan.style.marginBottom = value * 0.4 + 'px';
+    viento.style.left = value * 0.4 + 'px';
+    persona.style.marginLeft = value* 4 + 'px';
+
+}) 
+
+const navigation = document.querySelector('nav')
+document.querySelector('.menu').onclick = function (){
+    this.classList.toggle('active');
+    navigation.classList.toggle('active'); 
+
+}
